e91bce1188559274db486b8151e447f44dfdb553
Pawel F <pawelekflis5@gmail.com>
2023-10-09 16:39:11 +0200

    Fixing and updating laravel with docker

--- docker/nginx/docker-nginx-entrypoint.sh
+++ docker/nginx/docker-nginx-entrypoint.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-openssl req -x509 -nodes -newkey rsa:4096 -days 30\
-    -keyout "/etc/nginx/data/privkey.pem" \
-    -out "/etc/nginx/data/fullchain.pem" \
-    -subj "/CN=App"
-
 exec "$@"
